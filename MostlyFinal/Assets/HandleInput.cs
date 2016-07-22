783ac48b4de91d6b836ae3f781b787390f14ffef
AnanthChristy <ananthchristy@Ananths-MacBook-Air.local>
2016-07-22 15:12:24 -0700

    Capstone Project First Commit
    Contains working background and an empty cube ready for
    htmlTexturePlugin.

---
+++ MostlyFinal/Assets/HandleInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (GvrViewer.Instance.Tilted || GvrViewer.Instance.Triggered) {
+			Application.Quit();
+		}
+	}
+}
---
+++ MostlyFinal/Assets/Spinner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+	public float degreesPerSecond;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.up * Time.deltaTime * degreesPerSecond);
+	}
+}
